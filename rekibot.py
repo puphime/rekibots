1289e1de288c6a291e0ebab5a3b80b912e26014d
Aneta Arciszewska <aneta.arciszewska@nac.gov.pl>
2020-01-12 11:23:18 +0100

    move keeping track of what was posted to db for persistence, add migration mechanism, add extra tags column in db

--- rekibot.py
+++ rekibot.py
@@ -84,7 +84,6 @@ class danboorubot(ananas.PineappleBot):
     def check_booru(self):
         conn = sqlite3.connect("%s.db" % self.config._name)
         cur = conn.cursor()
-        before_queue_length=len(self.queue)
         badpages=0
         for page in range(1,301):
             while True:
@@ -112,7 +111,7 @@ class danboorubot(ananas.PineappleBot):
                     else:
                         continue
                     try:
-                        cur.execute(self.insert_sql, (int(post['id']),danbooru_url,source_url))
+                        cur.execute(self.insert_sql, (int(post['id']),danbooru_url,source_url,post['tag_string']))
                         conn.commit()
                     except:
                         pass
@@ -127,14 +126,6 @@ class danboorubot(ananas.PineappleBot):
                 print("[{0:%Y-%m-%d %H:%M:%S}] 5 bad pages in a row. Break processing.".format(datetime.now()), file=self.log_file, flush=True)
                 conn.close()
                 break
-        conn = sqlite3.connect("%s.db" % self.config._name)
-        cur = conn.cursor()
-        cur.execute(self.select_sql)
-        qtemp = cur.fetchall()
-        if len(qtemp)>before_queue_length:
-            self.queue = qtemp
-            print("[{0:%Y-%m-%d %H:%M:%S}] Queue now has {1} entries.".format(datetime.now(),len(self.queue)), file=self.log_file, flush=True)
-        conn.close()       
     
     def start(self):
         self.log_file = open("%s.log" % self.config._name, "a")
@@ -147,42 +138,77 @@ class danboorubot(ananas.PineappleBot):
         self.h = HTMLParser()
         self.queue=[]
         self.playlist=[]
-        self.excluded_tags = ['female_pervert','groping','breast_grab','pervert','sexual_harassment','sexually_suggestive','underwear_only','breast_press','topless','dangerous_beast','bottomless','no_panties','spoilers','revealing_clothes','pet_play','eargasm','daijoubu?_oppai_momu?','guro','bdsm','bondage','foot_worship','comic','cameltoe','osomatsu-san','osomatsu-kun','naked_sheet','foot_licking','nude','nude_cover','bunnysuit','randoseru','age_difference','younger','child','incest','you_gonna_get_raped','sisters','kindergarten_uniform','male_focus','1boy','multiple_boys','violence','horror','parody','no_humans','calne_ca','predator','goron','ichigo_mashimaro','manly','upskirt','banned_artist']
+        self.excluded_tags = ['female_pervert','groping','breast_grab','pervert','sexual_harassment','sexually_suggestive','underwear_only','breast_press','topless','dangerous_beast','bottomless','no_panties','spoilers','revealing_clothes','pet_play','eargasm','daijoubu?_oppai_momu?','guro','bdsm','bondage','foot_worship','comic','cameltoe','osomatsu-san','osomatsu-kun','naked_sheet','foot_licking','nude','nude_cover','bunnysuit','randoseru','age_difference','younger','child','incest','you_gonna_get_raped','sisters','kindergarten_uniform','male_focus','1boy','multiple_boys','violence','horror','parody','no_humans','calne_ca','predator','goron','ichigo_mashimaro','manly','upskirt','banned_artist','santa_costume']
         self.mandatory_tags = ['girl',]
 
         conn = sqlite3.connect("%s.db" % self.config._name)
         cur = conn.cursor()
-        self.create_table_sql = """ create table if not exists images (
+        self.create_table_sql = """create table if not exists images (
                                 danbooru_id integer primary key,
                                 url_danbooru text,
                                 url_source text,
-                                blacklisted integer,
+                                tags text,
+                                posted integer default 0,
+                                blacklisted integer default 0,
                                 UNIQUE(url_danbooru),
                                 UNIQUE(url_source)
                                );"""
-        self.insert_sql = "insert into images(danbooru_id,url_danbooru,url_source,blacklisted) values(?,?,?,0);"
-        self.select_sql = "select danbooru_id,url_danbooru,url_source from images where blacklisted=0;"
-        self.update_sql = "update images set blacklisted=1 where danbooru_id=?;"
-        
+        self.insert_sql = "insert into images(danbooru_id,url_danbooru,url_source,tags) values(?,?,?,?);"
+        self.select_sql = "select danbooru_id,url_danbooru,url_source from images where blacklisted=0 and posted=0;"
+        self.blacklist_sql = "update images set blacklisted=1 where danbooru_id=?;"
+        self.unmark_sql = "update images set posted=0;"
+        self.mark_sql = "update images set posted=1 where danbooru_id=?;"
+        self.migrate_db_sql1 = "alter table images rename to images_old;"
+        self.migrate_db_sql2 = "update images set blacklisted=1 where danbooru_id in (select danbooru_id from images_old where blacklisted=1);"
+        self.migrate_db_sql3 = "update images set posted=1 where danbooru_id in (select danbooru_id from images_old where posted=1);"
+        self.migrate_db_sql4 = "drop table images_old;"
+
+        if self.config.migratedb == "yes":
+            print("[{0:%Y-%m-%d %H:%M:%S}] ALTER TABLE.".format(datetime.now()), file=self.log_file, flush=True)
+            cur.execute(self.migrate_db_sql1)
         cur.execute(self.create_table_sql)
         conn.commit()
-               
-        cur.execute(self.select_sql)
-        self.queue = cur.fetchall()
         conn.close()
-        print("[{0:%Y-%m-%d %H:%M:%S}] Queue now has {1} entries.".format(datetime.now(),len(self.queue)), file=self.log_file, flush=True)
         self.check_booru()
+        if self.config.migratedb == "yes":
+            conn = sqlite3.connect("%s.db" % self.config._name)
+            cur = conn.cursor()
+            print("[{0:%Y-%m-%d %H:%M:%S}] UPDATE.".format(datetime.now()), file=self.log_file, flush=True)
+            cur.execute(self.migrate_db_sql2)
+            print("[{0:%Y-%m-%d %H:%M:%S}] UPDATE.".format(datetime.now()), file=self.log_file, flush=True)
+            try:
+                cur.execute(self.migrate_db_sql3)
+            except Exception as e:
+                print("[{0:%Y-%m-%d %H:%M:%S}] {1}".format(datetime.now(),e), file=self.log_file, flush=True)
+            print("[{0:%Y-%m-%d %H:%M:%S}] DROP TABLE.".format(datetime.now()), file=self.log_file, flush=True)
+            cur.execute(self.migrate_db_sql4)
+            conn.commit()
+            conn.close()
+            print("[{0:%Y-%m-%d %H:%M:%S}] MIGRATE OK.".format(datetime.now()), file=self.log_file, flush=True)
+            self.config.migratedb="no"
 
     @ananas.schedule(minute="*")
     def post(self):
         if datetime.now().minute != int(self.config.offset) and datetime.now().minute != int(self.config.offset)+30: 
             return
         while True:
-            if len(self.playlist) == 0:
-                self.playlist = self.queue
-                random.shuffle(self.playlist)
-                print("[{0:%Y-%m-%d %H:%M:%S}] Refilled playlist with {1} entries.".format(datetime.now(),len(self.playlist)), file=self.log_file, flush=True)
-            id,url,src = self.playlist.pop()
+            while len(self.queue) == 0:
+                conn = sqlite3.connect("%s.db" % self.config._name)
+                cur = conn.cursor()
+                cur.execute(self.select_sql)
+                self.queue = cur.fetchall()
+                random.shuffle(self.queue)
+                self.queue = self.queue[:100]
+                if len(self.queue) == 0:
+                    print("[{0:%Y-%m-%d %H:%M:%S}] No valid entries. Resetting db.".format(datetime.now()), file=self.log_file, flush=True)
+                    cur.execute(self.unmark_sql)
+                    conn.commit()
+                else:
+                    cur.executemany(self.mark_sql, [(str(item[0]),) for item in self.queue])
+                    conn.commit()
+                conn.close()
+                print("[{0:%Y-%m-%d %H:%M:%S}] Refilled queue with {1} entries.".format(datetime.now(),len(self.queue)), file=self.log_file, flush=True)
+            id,url,src = self.queue.pop()
             try:
                 url = urllib.request.urlretrieve(url)[0]
                 with open(url,'rb') as file:
@@ -190,7 +216,7 @@ class danboorubot(ananas.PineappleBot):
                 status_text = 'http://danbooru.donmai.us/posts/{0}\r\nsource: {1}'.format(id,src)
                 self.mastodon.status_post(status_text, in_reply_to_id=None, media_ids=(mediadict['id'],), sensitive=True, visibility="unlisted", spoiler_text=None)
             except Exception as e:
-                print("[{0:%Y-%m-%d %H:%M:%S}] {1}".format(datetime.now(),e), file=self.log_file, flush=True)
+                print("[{0:%Y-%m-%d %H:%M:%S}] Post id {1} {2}".format(datetime.now(),id,e), file=self.log_file, flush=True)
                 continue
             else:
                 print("[{0:%Y-%m-%d %H:%M:%S}] Posted.".format(datetime.now()), file=self.log_file, flush=True)
@@ -212,7 +238,7 @@ class danboorubot(ananas.PineappleBot):
                 id = id.groups()[0]
                 conn = sqlite3.connect("%s.db" % self.config._name)
                 cur = conn.cursor()
-                cur.execute(self.update_sql, (id,))
+                cur.execute(self.blacklist_sql, (id,))
                 conn.commit()
                 conn.close()
                 print("[{0:%Y-%m-%d %H:%M:%S}] Blacklisted {1}".format(datetime.now(),id), file=self.log_file, flush=True)
